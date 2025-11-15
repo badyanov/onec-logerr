e0cda9513b39274a88656dc1686ed47a7b47c3fb
Dmitriy Badyanov <d.badyanov@outlook.com>
2025-11-15 22:24:28 +0400

    Первая версия стартовой страницы и обработчик контроллера

--- src/main/java/xs/badyanov/onec_logerr/controller/IndexController.java
+++ src/main/java/xs/badyanov/onec_logerr/controller/IndexController.java
@@ -2,12 +2,14 @@ package xs.badyanov.onec_logerr.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @GetMapping("/")
     public String indexPage(Model model) {
-        return "Hello, world!";
+        return "index";
     }
 
 }
