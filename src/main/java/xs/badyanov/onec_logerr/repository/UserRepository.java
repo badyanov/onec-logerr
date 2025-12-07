cddc4bff5733eea962aae3998fb47f142a0ac7a7
Dmitriy Badyanov <d.badyanov@outlook.com>
2025-12-07 23:08:33 +0400

    Реализован метод для поиска пользователя по значениям полей

--- src/main/java/xs/badyanov/onec_logerr/repository/UserRepository.java
+++ src/main/java/xs/badyanov/onec_logerr/repository/UserRepository.java
@@ -1,12 +1,23 @@
 package xs.badyanov.onec_logerr.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import xs.badyanov.onec_logerr.entity.AppUser;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByUsernameIgnoreCase(String username);
-    
+
+    @Query("""
+            SELECT u FROM AppUser u
+            WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                OR LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                OR LOWER(u.telegram) LIKE LOWER(CONCAT('%', :keyword, '%'))
+            """)
+    List<AppUser> search(String keyword, Sort sort);
+
 }
