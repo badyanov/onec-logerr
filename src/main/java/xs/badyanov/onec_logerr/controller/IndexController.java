7a7139b35cbbfe802d9cc4f44481eccf33d94a7f
Dmitriy Badyanov <d.badyanov@outlook.com>
2025-12-07 21:16:52 +0400

    Добавлен логгер

--- src/main/java/xs/badyanov/onec_logerr/controller/IndexController.java
+++ src/main/java/xs/badyanov/onec_logerr/controller/IndexController.java
@@ -1,5 +1,7 @@
 package xs.badyanov.onec_logerr.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @GetMapping("/")
     public String indexPage(Model model) {
+        logger.debug("Handling GET request to /");
         return "index";
     }
 
